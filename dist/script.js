162b1dcb8cfbdd1813e107b81fb53057f8aae602
justusberner <justus.berner@gmail.com>
2021-08-26 10:25:47 +0300

    v.0.1

--- dist/script.js
+++ dist/script.js
@@ -24,7 +24,7 @@
 						dot.className += " correct";
 					});
 					document.body.className += " correct";
-					window.location.
+					window.location.href="https://app.livelokaatio.fi/?page_id=22";
 
 				}
 				setTimeout(function() {
